c988102c01afee69a076ec206f9729d78d5dbfdd
santoshpavan <santoshpavan.psp@gmail.com>
2020-10-12 17:05:21 -0400

    heap memory allocation

--- paging/vgetmem.c
+++ paging/vgetmem.c
@@ -14,9 +14,35 @@ extern struct pentry proctab[];
 WORD	*vgetmem(nbytes)
 	unsigned nbytes;
 {
+	kprintf("---VGETMEM\n");
 
-	kprintf("To be implemented!\n");
-	return( SYSERR );
+	STATWORD ps;
+        struct  mblock  *p, *q, *leftover;
+
+        disable(ps);
+        struct mblock v_heap_memlist = &proctab[currpid].vmemlist;
+	if (nbytes==0 || v_heap_memlist->mnext== (struct mblock *) NULL) {
+                restore(ps);
+                return( (WORD *)SYSERR);
+        }
+        nbytes = (unsigned int) roundmb(nbytes);
+        for (q=v_heap_memlist,p=v_heap_memlist->mnext ;
+             p != (struct mblock *) NULL ;
+             q=p,p=p->mnext)
+                if ( p->mlen == nbytes) {
+                        q->mnext = p->mnext;
+                        restore(ps);
+                        return( (WORD *)p );
+                } else if ( p->mlen > nbytes ) {
+                        leftover = (struct mblock *)( (unsigned)p + nbytes );
+                        q->mnext = leftover;
+                        leftover->mnext = p->mnext;
+                        leftover->mlen = p->mlen - nbytes;
+                        restore(ps);
+                        return( (WORD *)p );
+                }
+        restore(ps);
+        return( (WORD *)SYSERR );
 }
 
 
