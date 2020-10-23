0524f62b5e71b3bbd59540d622d5ce573fe8294a
santoshpavan <santoshpavan.psp@gmail.com>
2020-10-23 03:45:11 -0400

    get_bs shared implemented properly

--- paging/get_bs.c
+++ paging/get_bs.c
@@ -10,13 +10,25 @@ int get_bs(bsd_t bs_id, unsigned int npages) {
 	kprintf("---get_bs!\n");
 	int bs_ind = (int)bs_id;
 	if (npages <= 0 || npages > MAX_BST_SIZE || bs_ind < 0 || bs_ind >= NBSM ) {
-		return SYSERR
+		return SYSERR;
 	}
-	if (proctab[currpid].pvtproc == IS_PRIVATE) {
-        return SYSERR;
+    struct bs_map_t *pmap = &bsm_tab[bs_ind];
+    struct pentry *pproc = &proctab[currpid];    
+	if (pproc->pvt == IS_PRIVATE) {
+        if (pmap->status == BS_MAPPED && pmap->bs_pid == currpid && pmap->pvt == IS_PRIVATE) {
+            return MAX_BST_SIZE - pmap->bs_npages;
+        }
+        else if (pmap->status == BS_UNMAPPED) {
+            return npages;
+        }
     }
-	if (bsm_tab[bs_ind].bs_status == BS_MAPPED) {
-		return (MAX_BST_SIZE - bsm_tab[bs_ind].bs_npages);
-	}
-	return npages;
+    else {
+    	if (pmap->pvt == NOT_PRIVATE) {
+            if (pmap->status == BS_MAPPED)
+    		    return (MAX_BST_SIZE - bsm_tab[bs_ind].bs_npages) >= npages ? npages : SYSERR;
+            else
+                return npages;
+    	}
+    }
+	return SYSERR;
 }
